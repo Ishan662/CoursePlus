3e7e03ad820eaf7f06e9ccd3f03f91caf797828e
Ishan Maduranga <ishanmadurangai662@gmail.com>
2026-01-09 10:35:19 +0530

    resolved the saving errors of DTo and controller

--- backend/CoursePlus/Controllers/ChoiceController.cs
+++ backend/CoursePlus/Controllers/ChoiceController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoursePlus.Application.Interfaces.QuestionsChoice;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoursePlus.API.Controllers
 {
-    public class ChoiceController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ChoiceController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IChoiceService service;
+
+        public ChoiceController(IChoiceService service)
         {
-            return View();
+            this.service = service;
         }
     }
 }
